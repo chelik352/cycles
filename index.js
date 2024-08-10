acf0e6edaa292f27202bbc7807d5e3f29ea6391d
chelik352 <marmak11@bk.ru>
2024-08-10 15:54:55 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+1.
+    let num = 0;
+    while (num < 5) {
+        console.log(num);
+        num++;
+    }
+
+    do {
+        console.log(num);
+        num++;
+    }   while (num < 5);
+
+    for (let num = 0; num < 6; num++) {
+        console.log(num)
+    }
+
+    for (let  num = 0; num < 10; num++) {
+        console.log(num);
+        if (num == 5) break;
+    }
+    console.log("its 5")
+
+    for (let num = 0; num < 10; num++) {
+        if (num == 4) continue;
+        console.log(num);
+    }
+2.
+    false
+3.
+    let shf = 0
+    while (shf < 3) {
+        console.log(shf);
+        shf++;
+    }
+4.
+    for (let hjf = 0; hjf < 2; hjf++) {
+        console.log(hjf);
+        if (hjf == 1) break;
+    }
+    console.log("jfj")
+
+    for (let size = 0; size < 3; size++) {
+        console.log(size);
+        if (size == 1) break;
+    }
+    console.log("jfj")
+
